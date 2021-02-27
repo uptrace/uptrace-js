1cce816f026ba1ff68be87b4fd1a28c334760a29
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2021-02-27 13:54:52 +0200

    Add OTLP example

---
+++ example/otlp-node/README.md
@@ -0,0 +1,17 @@
+# Using OTLP exporter with Uptrace
+
+This example shows how to configure
+[OTLP](https://github.com/open-telemetry/opentelemetry-js/tree/main/packages/opentelemetry-exporter-collector) to export traces
+to Uptrace.
+
+Install dependencies:
+
+```shell
+yarn install
+```
+
+Run:
+
+```go
+UPTRACE_TOKEN=<token> node main.js
+```
---
+++ example/otlp-node/main.js
@@ -0,0 +1,48 @@
+const otel = require('@opentelemetry/api')
+const { BatchSpanProcessor } = require('@opentelemetry/tracing')
+const { NodeTracerProvider } = require('@opentelemetry/node')
+const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector')
+
+const exporter = new CollectorTraceExporter({
+  url: 'https://otlp.uptrace.dev/v1/traces',
+  headers: {
+    'uptrace-token': process.env.UPTRACE_TOKEN,
+  },
+
+  serviceName: 'myservice',
+  serviceVersion: '1.0.0',
+})
+const bsp = new BatchSpanProcessor(exporter, {
+  maxExportBatchSize: 1000,
+  maxQueueSize: 1000,
+})
+
+const provider = new NodeTracerProvider()
+provider.addSpanProcessor(bsp)
+provider.register()
+
+const tracer = otel.trace.getTracer('app_or_package_name', '1.0.0')
+
+const main = tracer.startSpan('main')
+otel.context.with(otel.setSpan(otel.context.active(), main), () => {
+  const child1 = tracer.startSpan('child1')
+  otel.context.with(otel.setSpan(otel.context.active(), child1), () => {
+    child1.setAttribute('key1', 'value1')
+    child1.recordException(new Error('error1'))
+    child1.end()
+  })
+
+  const child2 = tracer.startSpan('child2')
+  otel.context.with(otel.setSpan(otel.context.active(), child1), () => {
+    child2.setAttribute('key2', 42)
+    child2.end()
+  })
+
+  console.log('trace id:', main.context().traceId)
+  main.end()
+})
+
+// Flush the buffers.
+setTimeout(async () => {
+  await bsp.shutdown()
+})
