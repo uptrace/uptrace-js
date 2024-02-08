550fe0c4286638de020e54741a9d527b0ec3b80a
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2024-02-08 13:57:25 +0200

    chore: update examples

---
+++ example/otlp-metrics/README.md
@@ -0,0 +1,19 @@
+# Using OTLP exporter with Uptrace
+
+This example shows how to configure
+[OTLP](https://github.com/open-telemetry/opentelemetry-js/tree/main/packages/opentelemetry-exporter-collector)
+to export metrics to Uptrace.
+
+Install dependencies:
+
+```shell
+npm install
+```
+
+To run this example, you need to
+[create an Uptrace project](https://uptrace.dev/get/get-started.html) and pass your project DSN via
+`UPTRACE_DSN` env variable:
+
+```go
+UPTRACE_DSN=https://<token>@api.uptrace.dev/<project_id> node main.js
+```
---
+++ example/otlp-metrics/main.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const otel = require('@opentelemetry/api')
+const { Resource } = require('@opentelemetry/resources')
+const { NodeSDK } = require('@opentelemetry/sdk-node')
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http')
+const {
+  PeriodicExportingMetricReader,
+  AggregationTemporality,
+} = require('@opentelemetry/sdk-metrics')
+
+const dsn = process.env.UPTRACE_DSN
+console.log('using dsn:', dsn)
+
+const exporter = new OTLPMetricExporter({
+  url: 'https://otlp.uptrace.dev/v1/metrics',
+  headers: { 'uptrace-dsn': dsn },
+  compression: 'gzip',
+})
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: exporter,
+  exportIntervalMillis: 15000,
+})
+
+const sdk = new NodeSDK({
+  metricReader: metricReader,
+  resource: new Resource({
+    'service.name': 'myservice',
+    'service.version': '1.0.0',
+  }),
+})
+sdk.start()
+
+const meter = otel.metrics.getMeter('app_or_package_name', '1.0.0')
+
+const requestCounter = meter.createCounter('requests', {
+  description: 'Example of a Counter',
+})
+
+const upDownCounter = meter.createUpDownCounter('test_up_down_counter', {
+  description: 'Example of a UpDownCounter',
+})
+
+const attributes = { environment: 'staging' }
+
+setInterval(() => {
+  requestCounter.add(1, attributes)
+  upDownCounter.add(Math.random() > 0.5 ? 1 : -1, attributes)
+}, 1000)
