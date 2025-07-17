bfbf03a8fb87d775d19392fbf5af6e0100f8f17f
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2025-07-17 16:53:14 +0300

    .

--- packages/uptrace-node/test/uptrace.test.ts
+++ packages/uptrace-node/test/uptrace.test.ts
@@ -1,34 +1,10 @@
 import * as assert from 'assert'
-import * as sinon from 'sinon'
 
 import { parseDsn } from '@uptrace/core'
-import { configureOpentelemetry } from '../src'
 
-describe('configureOpentelemetry', () => {
-  it('logs on empty dsn', () => {
-    const spy = sinon.spy(console, 'error')
-
-    configureOpentelemetry({ dsn: '' })
-
-    spy.restore()
-    assert.ok(spy.called)
-    assert.match(spy.args[0][1], /either dsn option or UPTRACE_DSN is required/)
-  })
-
-  it('logs on invalid dsn', () => {
-    const spy = sinon.spy(console, 'error')
-
-    configureOpentelemetry({ dsn: 'foo bar' })
-
-    spy.restore()
-    assert.ok(spy.called)
-    assert.match(spy.args[0][1], /can't parse DSN="foo bar"/)
-  })
-})
-
-describe('configureOpentelemetry', () => {
+describe('parseDsn', () => {
   it('supports app and otlp addr', () => {
-    let dsn = parseDsn('https://<key>@uptrace.dev/<project_id>')
+    let dsn = parseDsn('https://<key>@uptrace.dev?grpc=4317')
     assert.equal(dsn.siteUrl(), 'https://app.uptrace.dev')
     assert.equal(dsn.otlpHttpEndpoint(), 'https://api.uptrace.dev')
 
